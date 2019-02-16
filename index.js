75164f9b16a8f90c8fa2c64609c2bf079b113017
Fidalgo <pedrofidalgo7@gmail.com>
2019-02-16 21:37:12 +0000

    commented code

--- index.js
+++ index.js
@@ -3,26 +3,35 @@ const http = require('http');
 const url = require('url');
 
 // Using syncchronous here because this only gets read when the server stard
+// read the json data
 const json = fs.readFileSync(`./city.list.json`, 'utf-8');
+
+// parse the json data into an array
 const locationData = JSON.parse(json);
 
 // Creating server with http package
 const server = http.createServer((req, res) => {
+  // get the path name using the url package
   const pathName = url.parse(req.url, true).pathname;
+  // get what is sent on the url q parameter, for example www.url.com/?q=aveiro
   const name = url.parse(req.url, true).query.q;
 
   // --- ROUTES ---
-  // if route equals products
+  // if route equals root and there is data on the name variable
   if (pathName === '/' && name) {
+    // filter the json file to save on the results variable what matches the query made
     const results = locationData.filter(item => {
+      // regex to search for case insensitive data
       const regex = new RegExp(name, 'gi');
       return item.name.match(regex) || item.country.match(regex);
     });
-    console.log(results);
+    // if nothing matches
     if (results.length === 0) {
       res.writeHead(404, { 'Content-type': 'text/html' });
       res.end('Url was not found on the server!');
-    } else {
+    }
+    // if matches, return json data
+    else {
       res.writeHead(200, { 'Content-type': 'application/json' });
       res.end(JSON.stringify(results));
     }
